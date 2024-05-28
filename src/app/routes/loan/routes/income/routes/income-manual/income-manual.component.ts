14c7bab801ec92fdb19590c1ee1e0dfe9b50989d
Matt Rossetta <matt@l5ui.com>
2024-05-28 10:52:23 -0400

    basic options for manual docs

--- src/app/routes/loan/routes/income/routes/income-manual/income-manual.component.ts
+++ src/app/routes/loan/routes/income/routes/income-manual/income-manual.component.ts
@@ -1,5 +1,6 @@
 import { Models } from '$shared';
 import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription, map } from 'rxjs';
 import { RouteApiService } from '../../shared/store/api/route-api.service';
@@ -15,10 +16,28 @@ export class IncomeManualComponent implements OnInit, OnDestroy {
   protected readonly LoadingState = Models.LoadingState;
   protected readonly IncomeVerificationMethod = Models.IncomeVerificationMethod;
 
+  protected options = [
+    { label: 'Option 1', value: 1 },
+    { label: 'Option 2', value: 2 },
+    { label: 'Option 3', value: 3 },
+  ];
+
+  protected form = this.fb.group({
+    dropdown: [this.options[0].value, Validators.required],
+  });
+
   constructor(
+    private fb: FormBuilder,
     private route: ActivatedRoute,
     protected routeApi: RouteApiService,
   ) {}
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log('Form submitted with value:', this.form.value.dropdown);
+      // Here you can handle form submission logic
+    }
+  }
 
   ngOnInit() {
     this.subscriptions.add(
--- src/app/routes/loan/routes/income/routes/income-manual/income-manual.module.ts
+++ src/app/routes/loan/routes/income/routes/income-manual/income-manual.module.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { DropdownModule } from 'primeng/dropdown';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { HelpPanelComponent, MasterPageModule } from '../../../../../../components';
 import { IrsVerifyActionComponent } from '../../components/irs-verify-action/irs-verify-action.component';
@@ -12,10 +15,14 @@ import { routing } from './income-manual.routes';
   declarations: [IncomeManualComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     routing,
     MasterPageModule,
     HelpPanelComponent,
+    ButtonModule,
     CardModule,
+    DropdownModule,
     ProgressSpinnerModule,
     PlaidVerifyActionComponent,
     IrsVerifyActionComponent,
